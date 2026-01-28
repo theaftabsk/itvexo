f09eed6120324102af0a7b691c403186e67f32e3
theaftabsk <aftabsk0005@gmail.com>
2026-01-28 21:45:06 +0530

    Cleanup: Remove redundant declaration file

--- src/types/maath-random.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'maath/random/dist/maath-random.esm' {
-  const random: any;
-  export default random;
-}
-
-declare module 'maath/*' {
-  const anyExport: any;
-  export default anyExport;
-}
